b399213493a92c324cbd3aa85219e8c5f8cba406
Huighlet_Eyram_Zowonu <121682213+Huighlet@users.noreply.github.com>
2024-08-01 21:46:34 +0000

    Delete apostrophe.py

--- apostrophe.py
+++
@@ -1,8 +0,0 @@
-message = "One of python's strenghts is its diverse community."
-print(message)
-
-
-#add underscore to large numbers to make it more readable
-
-universe_age = 14_000_000_000_000
-print(universe_age)
